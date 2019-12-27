c5e62cf0a01eea9d7ad576c17b759ed9df9903a6
Paweł Grajewski <unionproject88@gmail.com>
2019-12-27 23:53:54 +0100

    add weather route

--- headlines.py
+++ headlines.py
@@ -3,7 +3,15 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import json
+import urllib
+
 app = Flask(__name__)
+
+#https://openexchangerates.org/account/app-ids
+#https://home.openweathermap.org/api_keys
+
+exchange = "https://openexchangerates.org//api/latest.json?app_id=6649142c4d9b495bbe34570ae62d6355"
 
 RSS_FEEDS = {
     'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
@@ -21,6 +29,27 @@ def get_news():
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html",articles=feed['entries'])
+
+@app.route("/weather" , methods=['GET','POST'])
+def get_weather():
+    query = request.form.get("city")
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=01b4098397b9017a7f25bd7424b588bd"
+    #query = urllib.parse.quote(query)
+    print(query)
+    url = api_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description': parsed['weather'][0]['description'],
+                    'temperature': parsed['main']['temp'],
+                    'city': parsed['name'],
+                    'country': parsed['sys']['country']
+                    }
+    return render_template("weather.html",weather=weather)
+
+
+
 
 @app.route("/<publication>")
 def get_news1(publication="bbc"):
