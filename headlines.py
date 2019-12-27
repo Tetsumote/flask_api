ed5c8b8e7fecd7135546896dfd34ab939fb9f467
Paweł Grajewski <unionproject88@gmail.com>
2019-12-27 22:31:53 +0100

    add search form POST

--- headlines.py
+++ headlines.py
@@ -12,9 +12,9 @@ RSS_FEEDS = {
 }
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def get_news():
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
